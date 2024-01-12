5d04e359c013f3a0fd60142d8fbea9b310c60cb3
Freya Zhang <freyazhang@Freyas-MacBook-Air.local>
2024-01-11 21:00:33 -0500

    Updates to allow data batching

--- anki decks/generate-datasets.py
+++ anki decks/generate-datasets.py
@@ -4,7 +4,7 @@ import csv
 from datetime import datetime
 from random import *
 
-input_file_path = 'os_processed.csv'
+input_file_path = 'Computer Science_processed.csv'
 current_time = datetime.now()
 dotInd = input_file_path.find('.')
 output_file_path = input_file_path[:dotInd]+'-'+current_time.strftime("%H-%M-%S")+'.csv'
@@ -25,38 +25,51 @@ with open(input_file_path, mode = 'r', newline='') as infile, \
   for row in reader:
     inputs.append(row)
   
-  # for i in range(20):
+  # for i in range(200):
   for i in range(1):
 
-    # rewrite this as a batch request
-    shuffle(inputs)
+    req = []
 
-    num = randint(1, 10)
-    subset = inputs[:num]
+    # 10 datasets per request
+    for j in range(2):
 
-    print(f'Given output, it will make {num} cards.')
-    formattedSubset = []
+      # each dataset contains {num} cards 
+      num = randint(1, 10)
+      subset = inputs[:num]
+      inputs = inputs[num:]
 
-    for s in subset:
-      formattedSubset.append(f"[Front: {s[0]} Back: {s[1]}]")
+      print(f'Given output, it will make {num} cards.')
+      formattedSubset = []
 
-    cards = ",\n".join(formattedSubset)
+      for s in subset:
+        formattedSubset.append(f"[Front: {s[0]} Back: {s[1]}]")
 
-    writtenOrNah = 'written ' if randint(0, 1) == 1 else ''
+      cards = ",\n".join(formattedSubset)
+
+      writtenOrNah = 'written ' if randint(0, 1) == 1 else ''
+
+      req.append(cards)
+
+    dataset = ""
+    for count, value in enumerate(req):
+      dataset += f'Set {count+1}: {value}\n'
+    
+    print("_______")
+    print(dataset)
+    print("_______")
 
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo-16k",
       messages=[
         {"role": "system", "content": "You are a computer science professor, skilled in explaining complex programming concepts and can create computer science textbook excerpts from flashcards."},
-        {"role": "user", "content": f"Given the flashcards {cards}, create the {writtenOrNah}textbook notes as if the flashcards were created from them."},
-        {"role": "user", "content": f"say hello"}
+        {"role": "user", "content": f"Given the following sets of flashcards {dataset}, create the {writtenOrNah} textbook notes as if the flashcards were created from them. I will pay you $50 for the textbook content that you generate for every set so be sure to state your responses separately as a numbered list."}
       ]
     )
 
-    # message = completion.choices[0].message.content
+    message = completion.choices[0].message.content
     message = completion.choices[0].message.content
     print(len(completion.choices))
-    writer.writerow([message, cards])
+    writer.writerow([message, dataset])
 
 
 
